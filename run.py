8801a135549201c73e3c86c57e4ac2c6a7735a7c
Ubuntu <ubuntu@ip-172-31-41-22.ec2.internal>
2019-07-16 15:54:47 +0000

    advanced routing updated

--- run.py
+++ run.py
@@ -17,6 +17,19 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<member_name>")                           
+def about_member(member_name):
+    member = {}
+    
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+                
+            return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
@@ -41,5 +54,8 @@ https://www.youtube.com/watch?v=r1fo9iN8cfM
 
 Using If Statements Inside Our HTML
 https://www.youtube.com/watch?v=ooIRblr0BVc
+
+Advanced Routing
+https://www.youtube.com/watch?time_continue=26&v=x8VY_2A8ze4
 
 """
